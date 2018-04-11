3f0054ff908e9bfe4d9875ec39cda10b84fdf706
YXLONG <yangxiaolong3@jd.com>
2018-04-11 10:24:25 +0800

    none

--- app/redux/ActionTypes.js
+++ app/redux/ActionTypes.js
@@ -1,7 +1,3 @@
-export const CounterIncrease = 'CounterIncrease';
-export const CounterDecrease = 'CounterDecrease';
-
-
 export const StartFetchingArtCatalog = 'START_FETCHING_ART_CATALOG';
 export const FetchArtCatalogWithSuccess = 'FETCH_ART_CATALOG_WITH_SUCCESS';
 export const FetchArtCatalogWithError = 'FETCH_ART_CATALOG_WITH_ERROR';

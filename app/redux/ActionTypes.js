97b12b240e90e9d6364b9dab6a60f4bee358678c
YXLONG <yangxiaolong3@jd.com>
2018-04-11 10:25:13 +0800

    none

--- app/redux/ActionTypes.js
+++ app/redux/ActionTypes.js
@@ -2,7 +2,6 @@ export const StartFetchingArtCatalog = 'START_FETCHING_ART_CATALOG';
 export const FetchArtCatalogWithSuccess = 'FETCH_ART_CATALOG_WITH_SUCCESS';
 export const FetchArtCatalogWithError = 'FETCH_ART_CATALOG_WITH_ERROR';
 
-
 export const StartFetchingArtList = 'START_FETCHING_ART_List';
 export const FetchArtListWithSuccess = 'FETCH_ART_List_WITH_SUCCESS';
 export const FetchArtListWithError = 'FETCH_ART_List_WITH_ERROR';
